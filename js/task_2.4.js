d0d92900647460babb69800deb3cf4e364cd5db4
Vitalii Hromyk <hromykk@gmail.com>
2020-09-25 17:43:49 +0300

    Add tasks for module №2

---
+++ js/task_2.4.js
@@ -0,0 +1,53 @@
+// Напиши функцию formatString(string) которая принимает строку и форматирует ее если необходимо.
+
+// Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
+// Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
+// const formatString = function (string) {
+//   const maxLeng = 40;
+//   if (string.length < maxLeng) {
+//     return string;
+//   }
+//   if (string.length > maxLeng) {
+//     return string.slice(0, -(string.length - maxLeng)) + '...';
+//   }
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // вернется оригинальная строка
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // вернется форматированная строка
+
+// console.log(formatString('Curabitur ligula sapien.'));
+// // вернется оригинальная строка
+
+// console.log(
+//   formatString(
+//     'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+//   ),
+// );
+// // вернется форматированная строка
+
+function formatString(string, maxLength = 40) {
+  // Write code under this line
+  if (string.length < maxLength) {
+    return string;
+  }
+  if (string.length > maxLength) {
+    return string.slice(0, -(string.length - maxLength)) + "...";
+  }
+}
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+console.log(
+  formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30)
+);
+// 'Vestibulum facilisis, purus ne...'
